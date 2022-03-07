a820e69c08f7400c30f55930fc6ce6c0719ba230
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-07 19:51:13 +0100

    added Layout component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Children from "../../types/children";
+
+interface Props {
+  children: Children;
+}
+
+const Layout = ({ children }: Props): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+        <title>Drink it!</title>
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
