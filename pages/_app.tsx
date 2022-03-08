464b144b01e723a837a202f4f756d665d7d0a8b1
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-08 19:22:20 +0100

    fixed unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import "@fontsource/open-sans";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
