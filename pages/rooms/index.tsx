acdd16083565f5c690b7ee9f6609ad73ae1b65e4
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-08 09:35:09 +0100

    added colors and global styles file

---
+++ pages/rooms/index.tsx
@@ -0,0 +1,25 @@
+import { PageHolder } from "../../types/styles/global";
+
+const tempRooms = [
+  {
+    id: "wqeqwe",
+    creator: "username",
+    game: "piramide",
+  },
+  {
+    id: "wqeqwe",
+    creator: "username",
+    game: "remar",
+  },
+  {
+    id: "wqeqwe",
+    creator: "username",
+    game: "piramide",
+  },
+];
+
+const RoomList = (): JSX.Element => {
+  return <PageHolder></PageHolder>;
+};
+
+export default RoomList;
