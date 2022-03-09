15fff988555837a0cc29d2a151d765dce69d77ac
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-09 15:58:19 +0100

    removed test from pages folder

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import RegisterPage from ".";
+import RegisterPage from "../pages/accounts/register";
 
-describe("Giebn Register page", () => {
+describe("Given Register page", () => {
   describe("When it's instanciated", () => {
     test("Then it should render a heading with the text 'DRINK IT' and a link with the text 'Log in'", () => {
       const expectedHeading = "DRINK IT";
