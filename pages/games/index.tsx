6171ca3526f45db4deb8813e897e8eef82981c1c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-09 19:36:04 +0100

    added the base content for the game list page

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,4 +1,8 @@
+import styled from "styled-components";
+import Layout from "../../components/Layout/Layout";
+import { CenteredContainer } from "../../styles/global";
 import { APIGame } from "../../types/Game";
+import Header from "../../types/Header";
 
 const APIGames = [
   {
@@ -16,7 +20,23 @@ interface Props {
   games: APIGame[];
 }
 
-const GamesList = ({ games }: Props) => {};
+const GamesListHolder = styled.ul``;
+
+const GamesList = ({ games }: Props) => {
+  const header: Header = {
+    title: "SELECT A GAME",
+    subtitle: "GAMES LIST",
+  };
+
+  // const gamesToRender: JSX.Element[] = games.map((game: APIGame): JSX.Element => <Game game={game}/>)
+  return (
+    <Layout pageTitle="Games" header={header}>
+      <CenteredContainer>
+        <GamesListHolder>{"gamesToRender"}</GamesListHolder>
+      </CenteredContainer>
+    </Layout>
+  );
+};
 
 export const getStaticProps = () => {
   return {
