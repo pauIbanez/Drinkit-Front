9c2f02dd7176c6cd4f44bbb6b033f25e543900d0
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-09 19:38:48 +0100

    added back link

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import styled from "styled-components";
 import Layout from "../../components/Layout/Layout";
+import { lightWhite } from "../../styles/colors";
 import { CenteredContainer } from "../../styles/global";
 import { APIGame } from "../../types/Game";
 import Header from "../../types/Header";
@@ -20,7 +22,17 @@ interface Props {
   games: APIGame[];
 }
 
-const GamesListHolder = styled.ul``;
+const GamesListHolder = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const Back = styled.a`
+  color: ${lightWhite};
+  text-decoration: none;
+  font-size: 20px;
+`;
 
 const GamesList = ({ games }: Props) => {
   const header: Header = {
@@ -33,6 +45,9 @@ const GamesList = ({ games }: Props) => {
     <Layout pageTitle="Games" header={header}>
       <CenteredContainer>
         <GamesListHolder>{"gamesToRender"}</GamesListHolder>
+        <Link href={"/"} passHref>
+          <Back>Back</Back>
+        </Link>
       </CenteredContainer>
     </Layout>
   );
