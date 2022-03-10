4d5d80af21ede842b8fbca78ed0c76d1b3bdea1d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-10 10:36:42 +0100

    added the msw handler

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { rest } from "msw";
-import { APIGames } from "../SharedTestObjects";
+import { APIGames, APIRooms } from "../SharedTestObjects";
 
 export const handlers = [
   rest.get(`${process.env.NEXT_PUBLIC_API_URL}games/list`, (req, res, ctx) => {
@@ -7,6 +7,15 @@ export const handlers = [
       ctx.status(200),
       ctx.json({
         games: APIGames,
+      })
+    );
+  }),
+
+  rest.get(`${process.env.NEXT_PUBLIC_API_URL}rooms/list`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        rooms: APIRooms,
       })
     );
   }),
