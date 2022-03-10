952d7b96271e36359a655cf4052fb78b500bb54f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-10 10:15:37 +0100

    correctly configured msw and added whatwg-fetch

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { rest } from "msw";
-import { APIGames } from "../__tests__/SharedObjects";
+import { APIGames } from "../SharedTestObjects";
 
 export const handlers = [
   rest.get(`${process.env.NEXT_PUBLIC_API_URL}games/list`, (req, res, ctx) => {
