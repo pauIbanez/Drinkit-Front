b15a8d092e904bfec33feb349ea59d60cd571b80
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-10 11:20:10 +0100

    added value change test for registerForm

--- components/Forms/RegisterForm/RegisterForm.test.tsx
+++ components/Forms/RegisterForm/RegisterForm.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import RegisterForm from "./RegisterForm";
 
 describe("Given Registerform", () => {
@@ -28,32 +29,27 @@ describe("Given Registerform", () => {
     });
   });
 
-  // describe("When it's instanciated and the user submits the form", () => {
-  //   test("Then it should call dispatch", () => {
-  //     const labels = {
-  //       name: "Name",
-  //       lname: "Last name",
-  //       email: "Email",
-  //       username: "Username",
-  //       password: "Password",
-  //     };
-  //     const expectedButton = "Register";
+  describe("When it's instanciated and the user uses the form", () => {
+    test("Then it should update the values", () => {
+      const labels = {
+        name: "Name",
+        lname: "Last name",
+        email: "Email",
+        username: "Username",
+        password: "Password",
+      };
+      const expectedButton = "Register";
 
-  //     render(<RegisterForm />);
+      render(<RegisterForm />);
 
-  //     const foundName = screen.getByLabelText(labels.name);
-  //     const foundLName = screen.getByLabelText(labels.lname);
-  //     const foundEmail = screen.getByLabelText(labels.email);
-  //     const foundUsername = screen.getByLabelText(labels.username);
-  //     const foundPassword = screen.getByLabelText(labels.password);
-  //     const foundButton = screen.getByRole("button", { name: expectedButton });
+      const foundName: HTMLInputElement = screen.getByLabelText(labels.name);
+      const foundLName: HTMLInputElement = screen.getByLabelText(labels.lname);
 
-  //     userEvent.type(foundName, "a");
-  //     userEvent.type(foundLName, "a");
-  //     userEvent.type(foundEmail, "a");
-  //     userEvent.type(foundUsername, "a");
-  //     userEvent.type(foundPassword, "a");
-  //     userEvent.click(foundButton);
-  //   });
-  // });
+      userEvent.type(foundName, "name");
+      userEvent.type(foundLName, "last name");
+
+      expect(foundName.value).toBe("name");
+      expect(foundLName.value).toBe("last name");
+    });
+  });
 });
