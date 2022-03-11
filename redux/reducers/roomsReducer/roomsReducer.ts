b6ae27c2a68447b1dd2673d36ad954127a6b36f2
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-11 11:33:05 +0100

    added reducer cases

--- redux/reducers/roomsReducer/roomsReducer.ts
+++ redux/reducers/roomsReducer/roomsReducer.ts
@@ -1,6 +1,11 @@
 import Action from "../../../types/Action";
 import { APIRoom } from "../../../types/Room";
 import defaultAction from "../../actions/defaultAction";
+import roomActionTypes from "../../actions/rooms/roomActionTypes";
+import {
+  AddRoomAction,
+  LoadRoomsAction,
+} from "../../actions/rooms/types/RoomActions";
 
 const roomsReducer = (
   currentRooms: APIRoom[] = [],
@@ -9,7 +14,12 @@ const roomsReducer = (
   let newRooms: APIRoom[] = [];
 
   switch (action.type) {
-    case value:
+    case roomActionTypes.loadRooms:
+      newRooms = [...(action as LoadRoomsAction).rooms];
+      break;
+
+    case roomActionTypes.addRoom:
+      newRooms = [...currentRooms, (action as AddRoomAction).room];
       break;
 
     default:
