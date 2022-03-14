810624907167ac6b92b485461fb14a40e85055a7
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-14 15:25:58 +0100

    added deleteRoom action type

--- redux/actions/rooms/roomActionTypes.ts
+++ redux/actions/rooms/roomActionTypes.ts
@@ -3,6 +3,7 @@ import RoomActionTypes from "./types/RoomActionTypes";
 const roomActionTypes: RoomActionTypes = {
   loadRooms: "load-rooms",
   addRoom: "add-room",
+  deleteRoom: "delete-room",
 };
 
 export default roomActionTypes;
