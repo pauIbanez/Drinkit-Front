bd7af818a34b8c0a0c0b61f4aa8898f8c323e295
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-16 16:57:31 +0100

    added activation page in front

--- pages/accounts/activate/[token]/index.tsx
+++ pages/accounts/activate/[token]/index.tsx
@@ -6,19 +6,25 @@ import {
   InfoHolder,
   InfoTitle,
   Linkedin,
+  MainTitle,
 } from "../../../../styles/global";
 
 interface Props {
   activated: boolean;
+  error?: string;
 }
 
-const activationPage = ({ activated }: Props) => {
+const activationPage = ({ activated, error }: Props) => {
   return (
-    <>
-      {activated ? (
-        <Layout pageTitle="Activation">
-          <CenteredContainer>
-            <InfoHolder>
+    <Layout
+      pageTitle="Activation"
+      header={{ title: "REGISTER", subtitle: "WELCOME" }}
+    >
+      <MainTitle>DRINK IT</MainTitle>
+      <CenteredContainer>
+        <InfoHolder>
+          {activated ? (
+            <>
               <InfoTitle>Activation</InfoTitle>
               <Info>
                 Nisuuu, your user has been succesfully activated! Proceed to{" "}
@@ -26,14 +32,46 @@ const activationPage = ({ activated }: Props) => {
                   <Linkedin>Log in</Linkedin>
                 </Link>
               </Info>
-            </InfoHolder>
-          </CenteredContainer>
-        </Layout>
-      ) : (
-        <></>
-      )}
-    </>
+            </>
+          ) : (
+            <>
+              <InfoTitle>Activation failed</InfoTitle>
+              <Info>Your user could not be activated. Reason: </Info>
+              <Info>{error}</Info>
+            </>
+          )}
+        </InfoHolder>
+      </CenteredContainer>
+    </Layout>
   );
+};
+
+interface Params {
+  params: {
+    token: string;
+  };
+}
+
+export const getServerSideProps = async ({ params: { token } }: Params) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}accounts/activate/${token}`
+  );
+  const body = await response.json();
+
+  if (body.error) {
+    return {
+      props: {
+        activated: false,
+        error: body.message,
+      },
+    };
+  }
+
+  return {
+    props: {
+      activated: true,
+    },
+  };
 };
 
 export default activationPage;
