2e263dc4eb0f3110d59231feccfa5b2a95fb1650
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-17 18:24:27 +0100

    added test for loadUser reducer case

--- redux/reducers/userReducer/userReducer.test.ts
+++ redux/reducers/userReducer/userReducer.test.ts
@@ -1,3 +1,6 @@
+import Player from "../../../types/Player";
+import { LoadUserAction } from "../../actions/user/types/UserActions";
+import userActionTypes from "../../actions/user/userActionTypes.ts";
 import defaultUser from "./defaultUser";
 import userReducer from "./userReducer";
 
@@ -7,6 +10,40 @@ describe("Given userReducer", () => {
       const newUser = userReducer();
 
       expect(newUser).toEqual(defaultUser);
+    });
+  });
+
+  describe("When its instanciated with default user and a loadUser action with a new user", () => {
+    test("Then it should return the new user", () => {
+      const user: Player = {
+        id: defaultUser.id,
+        profile: {
+          friends: ["frien", "frien"],
+          incomingRequests: [],
+          stats: {
+            games: 35,
+            sips: 2000,
+          },
+          username: "elPapi",
+        },
+        info: {
+          avatar: {
+            staticUrl: "https://someurl.com/images/yes/muchreral.png",
+          },
+          email: "someemail@gmail.com",
+          lastName: "supa",
+          name: "mr.",
+        },
+      };
+
+      const action: LoadUserAction = {
+        type: userActionTypes.loadUser,
+        user,
+      };
+
+      const newUser = userReducer(defaultUser, action);
+
+      expect(newUser).toEqual(user);
     });
   });
 });
