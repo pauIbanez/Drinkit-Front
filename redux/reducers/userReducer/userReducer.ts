99e779fa4a8fafc39676574eaeb2b126ae429f7e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-17 17:18:10 +0100

    added reducer cases for the user actions

--- redux/reducers/userReducer/userReducer.ts
+++ redux/reducers/userReducer/userReducer.ts
@@ -1,6 +1,11 @@
 import Action from "../../../types/Action";
 import Player from "../../../types/Player";
 import defaultAction from "../../actions/defaultAction";
+import {
+  LoadUserAction,
+  UpdateUserAction,
+} from "../../actions/user/types/UserActions";
+import userActionTypes from "../../actions/user/userActionTypes.ts";
 import defaultUser from "./defaultUser";
 
 const userReducer = (
@@ -10,6 +15,18 @@ const userReducer = (
   let newUser: Player;
 
   switch (action.type) {
+    case userActionTypes.loadUser:
+      newUser = { ...(action as LoadUserAction).user };
+      break;
+
+    case userActionTypes.updateUser:
+      let tempNewUser = Object.assign(
+        (action as UpdateUserAction).change,
+        currentUser
+      );
+      newUser = JSON.parse(JSON.stringify(tempNewUser));
+      break;
+
     default:
       newUser = { ...currentUser };
       break;
