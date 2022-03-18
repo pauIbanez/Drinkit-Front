f624332ce29ee667f84c146e4c4bc125fe1f35cb
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-18 17:29:21 +0100

    added loadUserThunk

---
+++ redux/thunks/userThunks/userThunks.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from "redux";
+import { getLoadUserAction } from "../../actions/user/userActionCreators";
+
+const getLoadUserThunk = (token: string) => async (dispatch: Dispatch) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}accounts/my-account`,
+    {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const body = await response.json();
+
+  if (body.error) {
+    //handle errors
+    return;
+  }
+  dispatch(getLoadUserAction(body));
+};
+
+module.exports = { getLoadUserThunk };
