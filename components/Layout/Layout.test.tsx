df044cc297490de716859b02b1ba3c73f138844a
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-18 19:53:48 +0100

    tested mandatory login

--- components/Layout/Layout.test.tsx
+++ components/Layout/Layout.test.tsx
@@ -2,6 +2,14 @@ import { screen } from "@testing-library/react";
 import { renderInBocata } from "../../jest.setup";
 import Layout from "./Layout";
 
+const mockPush = jest.fn();
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: mockPush,
+    pathname: "",
+  }),
+}));
+
 describe("Given Layout component", () => {
   describe("When it's instanciated passing children", () => {
     test("Then it should render the children", () => {
@@ -35,6 +43,20 @@ describe("Given Layout component", () => {
 
       expect(foundButton).toBeInTheDocument();
       expect(foundTitle).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's instanciated and the user is not logged in", () => {
+    test("Then it should call router.push with route '/accounts/login'", () => {
+      const expectedPath = "/accounts/login";
+
+      window.localStorage.getItem = jest.fn().mockReturnValue(null);
+
+      const buttonText = "Test Button";
+      const Children = <button>{buttonText}</button>;
+      renderInBocata(<Layout>{Children}</Layout>);
+
+      expect(mockPush).toHaveBeenCalledWith(expectedPath);
     });
   });
 });
