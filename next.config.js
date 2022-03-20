1c48e43efbb33b7dafdc95905cf182c9cc040fa3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-20 19:24:54 +0100

    added image dominas to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["localhost", "drinkit-back.onrender.com"],
+  },
 };
 
 module.exports = nextConfig;
