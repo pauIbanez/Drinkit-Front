28017096d9f79fc4416b10a33c91ad08f8cfc460
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-22 15:24:08 +0100

    added base for piramideLobbyReducer

---
+++ redux/reducers/piramideLobbyReducer/piramideLobbyReducer.ts
@@ -0,0 +1,21 @@
+import Action from "../../../types/Action";
+import defaultAction from "../../actions/defaultAction";
+
+const piramideLobbyReducer = (
+  currentLobby = {},
+  action: Action = defaultAction
+) => {
+  let newLobby;
+
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+
+  return newLobby;
+};
+
+export default piramideLobbyReducer;
