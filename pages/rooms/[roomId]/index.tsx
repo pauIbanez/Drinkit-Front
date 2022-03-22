65ebbb78d56fcd25cab91bf9999415d069f89d3e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-22 16:21:00 +0100

    added users pannel

--- pages/rooms/[roomId]/index.tsx
+++ pages/rooms/[roomId]/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useContext, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,6 +9,8 @@ import WSContext from "../../../contexts/wsContext";
 import { getUpdateStateAction } from "../../../redux/actions/piramideLobby/piramideLobbyActionCreators";
 import { mainRed } from "../../../styles/colors";
 import { CenteredContainer } from "../../../styles/global";
+import { globalRadius } from "../../../styles/variables";
+import Player from "../../../types/Player";
 import State from "../../../types/State";
 
 interface Message {
@@ -31,6 +34,32 @@ const RoomCode = styled.p`
 const PlayerCounter = styled.p`
   margin: 0;
   font-size: 14px;
+`;
+
+const PlayersSection = styled.section`
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+`;
+
+const PlayerHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  height: 90px;
+  width: 70px;
+`;
+
+const Avatar = styled(Image)`
+  border-radius: ${globalRadius};
+  object-fit: contain;
+`;
+
+const Username = styled.p`
+  margin: 0;
+  color: white;
+  fnt-size: 13px;
 `;
 
 const LobbyPage = (): JSX.Element => {
@@ -60,6 +89,37 @@ const LobbyPage = (): JSX.Element => {
 
   console.log(piramideLobby);
 
+  let usersToRender: JSX.Element[] = [];
+  if (piramideLobby.connectedPlayers) {
+    const connectedToRender = piramideLobby.connectedPlayers.map(
+      (player: Player) => (
+        <PlayerHolder key={player.id}>
+          <Avatar
+            height={70}
+            width={70}
+            src={`${process.env.NEXT_PUBLIC_API_URL}avatars/${player.profile.avatar.staticUrl}`}
+            alt="user avatar"
+          />
+          <Username>{player.profile.username}</Username>
+        </PlayerHolder>
+      )
+    );
+    const missingPlayers = piramideLobby.maxPlayers - connectedToRender.length;
+
+    let restToRender: JSX.Element[] = [];
+
+    for (let i = 0; i < missingPlayers; i++) {
+      restToRender.push(
+        <PlayerHolder key={i}>
+          <Avatar height={70} width={70} src="/default.png" alt="user avatar" />
+          <Username>Waiting...</Username>
+        </PlayerHolder>
+      );
+    }
+
+    usersToRender = [...connectedToRender, ...restToRender];
+  }
+
   return (
     <Layout
       pageTitle="Piramide lobby"
@@ -75,6 +135,7 @@ const LobbyPage = (): JSX.Element => {
             size={{ height: 35, width: 100 }}
           />
         </HorizontalContainer>
+        <PlayersSection>{usersToRender}</PlayersSection>
       </CenteredContainer>
     </Layout>
   );
--- types/PiramideLobby.ts
+++ types/PiramideLobby.ts
@@ -1,5 +1,16 @@
+import Player from "./Player";
+
 interface PiramideLobby {
   id: string;
+  minPlayers: number;
+  maxPlayers: number;
+  jokers: boolean;
+  twoDecks: boolean;
+  leftovers: boolean;
+  modifiers: string[];
+
+  leader: Player;
+  connectedPlayers: Player[];
 }
 
 export default PiramideLobby;
