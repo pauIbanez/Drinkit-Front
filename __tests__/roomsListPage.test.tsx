0b34fa3a9e31e7f4df16e769629de301912f0015
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-01 21:26:50 +0200

    fixed non-passing test for roomList changes

--- __tests__/roomsListPage.test.tsx
+++ __tests__/roomsListPage.test.tsx
@@ -7,6 +7,13 @@ import { APIRoom } from "../types/Room";
 import * as redux from "react-redux";
 import { mockUrls } from "../mocks/mockUrls";
 
+const mockPush = jest.fn();
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: mockPush,
+  }),
+}));
+
 describe("Given getServerSideProps", () => {
   describe("When it's instanciated and the response is ok", () => {
     test("Then it should return an object with a property props that contains the recieved rooms", async () => {
@@ -56,27 +63,6 @@ describe("Given RoomsList page", () => {
       const foundLink = screen.getByRole("link", { name: expectedLink });
 
       expect(foundLink).toBeInTheDocument();
-    });
-  });
-
-  describe("When it's instanciated passing a room and it's clicked and then the button is clicked", () => {
-    test("Then it should display a button with the text 'Delete' and then call dispatch", () => {
-      const expectedButton = "Delete";
-      const mockDispatch = jest.fn();
-      jest.spyOn(redux, "useDispatch").mockReturnValue(mockDispatch);
-
-      renderInBocata(<RoomList rooms={APIRooms} />);
-
-      const foundRooms = screen.getAllByRole("listitem");
-      userEvent.click(foundRooms[0]);
-
-      const foundButton = screen.getByRole("button", { name: expectedButton });
-
-      expect(foundButton).toBeInTheDocument();
-
-      userEvent.click(foundButton);
-
-      expect(mockDispatch).toHaveBeenCalled();
     });
   });
 });
