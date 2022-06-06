89bd235ffe3658341531335ef24c52e6f8cdab43
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-06 16:51:23 +0200

    added popup background

--- components/PiramideLobbySettings/PiramideLobbySettings.tsx
+++ components/PiramideLobbySettings/PiramideLobbySettings.tsx
@@ -1,15 +1,43 @@
+import styled from "styled-components";
+import { backgroundBlue } from "../../styles/colors";
+import { globalRadius } from "../../styles/variables";
 import PiramideLobby from "../../types/PiramideLobby";
-
 interface PiramideLobbySettingsProps {
   lobbySettings: PiramideLobby;
   wsInstance: WebSocket;
+  onBackgroundClick(): void;
 }
+
+const Closer = styled.section`
+  display: flex;
+  position: absolute;
+  inset: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: rgba(0, 0, 0, 0.45);
+`;
+
+const Popup = styled.div`
+  background-color: ${backgroundBlue};
+  border-radius: ${globalRadius};
+
+  height: 543px;
+  width: 329px;
+`;
 
 const PiramideLobbySettings = ({
   lobbySettings,
   wsInstance,
+  onBackgroundClick,
 }: PiramideLobbySettingsProps) => {
-  return <p>asdasds</p>;
+  return (
+    <Closer>
+      <Popup></Popup>
+    </Closer>
+  );
 };
 
 export default PiramideLobbySettings;
