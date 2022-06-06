667531ec664a080ee455eef901dbd31bf78bb481
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-06 17:57:33 +0200

    added base for settings toggler

--- components/PiramideLobbySettings/PiramideLobbySettings.tsx
+++ components/PiramideLobbySettings/PiramideLobbySettings.tsx
@@ -1,8 +1,9 @@
-import { BaseSyntheticEvent } from "react";
+import { BaseSyntheticEvent, useState } from "react";
 import styled from "styled-components";
-import { backgroundBlue } from "../../styles/colors";
+import { backgroundBlue, lightWhite, mainTeal } from "../../styles/colors";
 import { globalRadius } from "../../styles/variables";
 import PiramideLobby from "../../types/PiramideLobby";
+
 interface PiramideLobbySettingsProps {
   lobbySettings: PiramideLobby;
   wsInstance: WebSocket;
@@ -24,9 +25,102 @@ const Closer = styled.section`
 const Popup = styled.div`
   background-color: ${backgroundBlue};
   border-radius: ${globalRadius};
+  color: white;
 
   height: 543px;
   width: 329px;
+
+  overflow-y: scroll;
+  padding: 30px;
+`;
+
+const SectionTitle = styled.h3`
+  font-size: 20px;
+  margin: 0;
+`;
+
+const SettingHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 10px 0 10px 0;
+`;
+
+const Setting = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SettingInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const SettingTitle = styled.h4`
+  margin: 0;
+  font-size: 14px;
+`;
+
+const SettingDescription = styled.p`
+  margin: 0;
+
+  font-size: 10px;
+  color: ${lightWhite};
+`;
+
+const ToggleHolder = styled.div`
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const toggleHeight = 20;
+const toggleWidth = 50;
+
+const ToggleBackground = styled.div`
+  position: relative;
+  height: ${toggleHeight}px;
+  width: ${toggleWidth}px;
+
+  border-radius: ${toggleHeight / 2}px;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+interface ToggleProps {
+  toggled: boolean;
+}
+
+const ToggleHandle = styled.div`
+  background-color: white;
+  width: ${toggleHeight}px;
+  height: ${toggleHeight}px;
+  position: absolute;
+  border-radius: ${toggleHeight / 2}px;
+
+  z-index: 2;
+
+  ${(props: ToggleProps) =>
+    props.toggled && `transform: translateX(${toggleWidth - toggleHeight}px);`}
+
+  transition: all 0.3s ease-in-out;
+`;
+
+const ToggleFilledBackground = styled.div`
+  position: absolute;
+  inset: 0;
+  width: 10px;
+  ${(props: ToggleProps) =>
+    props.toggled && `background-color: ${mainTeal}; width: ${toggleWidth}px;`}
+
+  border-radius: ${toggleHeight / 2}px;
+
+  z-index: 1;
+
+  transition: all 0.3s ease-in-out;
 `;
 
 const PiramideLobbySettings = ({
@@ -34,13 +128,38 @@ const PiramideLobbySettings = ({
   wsInstance,
   onBackgroundClick,
 }: PiramideLobbySettingsProps) => {
+  const [toggled, setToggled] = useState(false);
+
   return (
     <Closer onClick={onBackgroundClick}>
       <Popup
         onClick={(event: BaseSyntheticEvent) => {
           event.stopPropagation();
         }}
-      ></Popup>
+      >
+        <SectionTitle>Game Settings</SectionTitle>
+        <SettingHolder>
+          <Setting>
+            <SettingInfo>
+              <SettingTitle>Two Decks</SettingTitle>
+              <SettingDescription>
+                Play with double the cards! With two decks enabled you can host
+                games of up to 12 players.
+              </SettingDescription>
+            </SettingInfo>
+            <ToggleHolder>
+              <ToggleBackground
+                onClick={() => {
+                  setToggled(!toggled);
+                }}
+              >
+                <ToggleFilledBackground toggled={toggled} />
+                <ToggleHandle toggled={toggled} />
+              </ToggleBackground>
+            </ToggleHolder>
+          </Setting>
+        </SettingHolder>
+      </Popup>
     </Closer>
   );
 };
